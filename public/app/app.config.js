14d9fd6079040bd100481b4e1776424c86db59fe
Carlos de Dios <c.dedios@outlook.com>
2016-02-02 09:33:38 -0400

    Bootstrapping manually
    - To ensure having all modules loaded when the file gets imported

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,14 +1,25 @@
-import { Inject } from './utils/decorators';
 import io from 'socket.io/socket.io';
+import { Inject } from './utils/decorators';
 
-const appConfig = ($provide) => {
+const appConfig = ($provide, $stateProvider, $urlRouterProvider) => {
   const socketSingleton = io();
 
   $provide.provider('socket', function() {
     this.$get = () => socketSingleton;
   });
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('chat', {
+    url: '/',
+    template: '<app></app>'
+  }).state('chat.private', {
+    url: 'private/{userId}'
+  }).state('chat.room', {
+    url: 'room/{roomId}'
+  });
 }
 
-Inject('$provide')(appConfig);
+Inject('$provide', '$stateProvider', '$urlRouterProvider')(appConfig);
 
 export default appConfig;
--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,35 @@
-import 'angular-ui-router';
+import 'dogfalo/materialize';
 import angular from 'angular';
-import AppConfig from './app.config';
+import 'angular-ui-router';
 import AppComponent from './app.component';
+import AppConfig from './app.config';
 
 const appModule = angular
-  .module('app', [])
-  .config(AppConfig)
-  .directive('app', AppComponent);
+  .module('app', ['ui.router'])
+  .directive('app', AppComponent)
+  .config(AppConfig);
+
+let noAngularDom;
+
+angular.element(document).ready(() => {
+  const container = document.getElementById('app-container');
+
+  if (location.origin.match(/localhost/)) {
+    System.trace = true;
+    noAngularDom = container.cloneNode(true);
+  }
+
+  angular.bootstrap(container, [appModule.name], {
+    strictDi: true
+  });
+});
+
+export default appModule;
+
+export default function __unload() {  
+  const container = document.getElementById('app-container');
+  container.remove();
+  document.body.appendChild(noAngularDOM.cloneNode(true));
+};
 
 export default appModule;
