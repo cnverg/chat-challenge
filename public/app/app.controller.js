2a6bcb28aabfb4eb19dfeedf19f07b8aa5ddffd5
Carlos de Dios <c.dedios@outlook.com>
2016-02-02 23:11:21 -0400

    Style refactory
    - Adding and applying new styles
    - Changing layout... So many times I can't even count them
    - Renaming app

--- public/app/app.controller.js
+++ public/app/app.controller.js
@@ -4,32 +4,30 @@ import Constants from './utils/constants'
 
 const deepEquals = (ob1, ob2) => JSON.stringify(ob1) === JSON.stringify(ob2);
 
-@Inject('$scope', 'socket', '$cookies', 'GravatarFactory')
+@Inject('$scope', 'socket', '$cookies', 'GravatarFactory', '$state')
 export default class AppController {
-  constructor($scope, socket, $cookies, GravatarFactory) {
-    this.$scope = $scope;
-    this.$socket = socket;
-    this.$cookies = $cookies;
-    this.GravatarFactory = GravatarFactory;
+  constructor($scope, socket, $cookies, GravatarFactory, $state) {
+    $scope.selectedTarget = '';
 
+    Object.assign(this, { $scope, socket, $cookies, GravatarFactory, $state });
     $scope.user = this.cookieUser;
 
-    this.$socket.on(Constants.userUpdate, (users) => {
+    this.socket.on(Constants.userUpdate, (users) => {
       $scope.$apply(() => {
         $scope.users = users;
         if ($scope.user.isValid && !users.some(u => deepEquals(u, $scope.user))) {
-          this.$socket.emit(Constants.userEnter, $scope.user);          
+          this.socket.emit(Constants.userEnter, $scope.user);          
         }
       });
     });
 
-    this.$socket.on(Constants.roomUpdate, (rooms) => {
+    this.socket.on(Constants.roomUpdate, (rooms) => {
       $scope.$apply(() => {
         $scope.rooms = rooms;
       });
     });
 
-    this.$socket.on(Constants.message, () => {
+    this.socket.on(Constants.message, () => {
       console.log("message received");
     });
 
@@ -38,10 +36,6 @@ export default class AppController {
     $scope.leave = this.leave.bind(this);
     $scope.logIn = this.logIn.bind(this);
     $scope.logOut = this.logOut.bind(this);
-
-    $scope.$on('$destroy', () => {
-      this.logOut();
-    });
 
     $scope.notCurrentUser = (user) => user.id !== this.$scope.user.id;
   }
@@ -56,26 +50,28 @@ export default class AppController {
     this.$cookies.put(Constants.userCookie, JSON.stringify(user));
     this.$scope.user = this.cookieUser;
 
-    this.$socket.emit(Constants.userEnter, this.$scope.user);
+    this.socket.emit(Constants.userEnter, this.$scope.user);
   }
 
   logOut() {
-    this.$socket.emit(Constants.userLeave, this.$scope.user);
+    this.socket.emit(Constants.userLeave, this.$scope.user);
 
     this.$cookies.remove(Constants.userCookie);
     this.$scope.user = {};
+
+    this.$state.transitionTo('chat', null, { reload: true });
   }
 
   join(room) {
-    this.$socket.emit(Constants.join, room);
+    this.socket.emit(Constants.join, room);
   }
 
   leave(room) {
-    this.$socket.emit(Constants.leave, room);
+    this.socket.emit(Constants.leave, room);
   }
 
   send(room, message) {
-    this.$socket.emit(Constants.send, {
+    this.socket.emit(Constants.send, {
       room: room,
       message: message
     });
