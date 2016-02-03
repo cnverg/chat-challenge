f9d85253e7d9adffc1e3923888ae0027469b44d9
Carlos de Dios <c.dedios@outlook.com>
2016-02-03 15:41:58 -0400

    Room conversation is a thing
    * Do private conversations

--- public/app/components/chatroom/chatroom.controller.js
+++ public/app/components/chatroom/chatroom.controller.js
@@ -4,35 +4,45 @@ import Constants from '../../utils/constants';
 @Inject('$scope', '$stateParams', 'socket')
 export default class ClassroomController {
   constructor($scope, $stateParams, socket) {
-    console.log(socket);
-
     Object.assign(this, { $scope, socket });
 
+    $scope.messages = [];
+    let mostRecentMessage = {};
     $scope.$parent.selectedTarget = $scope.target = $stateParams.target;
 
-    socket.emit(Constants.join, $scope.target, $scope.user);
+    $scope.addMessage = $scope.messages.push.bind($scope.messages);
 
-    socket.on(Constants.message, (message) => {
-      console.log(message);
-    });
+    $scope.processMessage = (message) => {
+      if (mostRecentMessage.from && mostRecentMessage.from.id === message.from.id) {
+        mostRecentMessage.content.push(message.content);
+      } else {
+        message.content = message.content.split('\n');
+        $scope.addMessage(message);
+        mostRecentMessage = message;        
+      }
+    }
 
-    socket.on(Constants.greet, (user) => {
-      console.log(user);
-    });
+    $scope.safeAddMessage = (message) => $scope.$apply(() => $scope.addMessage(message));
+    $scope.safeProcessMessage = (message) => $scope.$apply(() => $scope.processMessage(message));
 
-    socket.on(Constants.grieve, (user) => {
-      console.log(user);
-    });
+    socket.on(Constants.greet, $scope.safeAddMessage);
+    socket.on(Constants.grieve, $scope.safeAddMessage);
+    socket.on(Constants.message, $scope.safeProcessMessage);
+
+    socket.emit(Constants.join, $scope.target);
 
     $scope.$on('$destroy', () => {
-      socket.emit(Constants.leave, $scope.target, $scope.user);
+      socket.emit(Constants.leave, $scope.target);
     });
 
     $scope.sendMessage = this.sendMessage.bind(this);
   }
 
   sendMessage() {
-    this.socket.emit(Constants.send, { room: this.$scope.chatroom, from: this.$scope.user, data: this.$scope.message });
+    const msg = { room: this.$scope.target, from: this.$scope.user, content: this.$scope.message, timestamp: Date.now() };
+    this.socket.emit(Constants.send, msg);
+
+    this.$scope.processMessage(msg);
 
     this.$scope.message = ''
     this.$scope.messageForm.$setPristine();
--- server.js
+++ server.js
@@ -8,10 +8,17 @@ var server  = require('http').createServer(app);
 var io      = require('socket.io')(server);
 
 var Constants = require('./public/app/utils/constants');
+var compose = require('./public/app/utils/helpers').compose;
 var users = [];
 
 const chatroomsDat = './resources/chatrooms.dat';
-var rooms = fs.readFileSync(chatroomsDat, 'utf8').split('\n').map(r => r.trim().replace(/ +/, '_'));
+
+const format = (str) => str.split('\n').map(r => r.trim().replace(/ +/g, '_'));
+
+const getChatroomData = compose(format, fs.readFileSync.bind(fs, chatroomsDat, 'utf8'));
+const writeToChatroomData = fs.writeFileSync.bind(fs, chatroomsDat);
+
+var rooms = getChatroomData();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -24,102 +31,117 @@ server.listen(3000, () => {
  * @param  {[Socket]} socket  socket in which the action will take effect
  * @return {[Unit]}           effectful void
  */
-const connection = (socket) => {
-  /**
-   * Allows the client to join a specified room
-   * @param  {[Socket]} socket  socket in which the action will take effect
-   * @param  {[String]} roomId  id of the room
-   * @return {[Unit]}           effectful void
-   */
-  const join = (roomId, user) => {
-    socket.broadcast.emit(Constants.leave, roomId, user);
+const connection = function(socket) {
+  const server = { fromSystem: true };
+  
+  // Returning IIFE to have private cache for each socket
+  return (function() {
+    let _user = {};
+    let _rooms = [];
+    /**
+     * Allows the client to join a specified room
+     * @param  {[String]} roomId  id of the room
+     * @return {[Unit]}           effectful void
+     */
+    const join = (roomId, user) => {
+      socket.join(roomId);
+      _rooms.push(roomId);
+      socket.broadcast.to(roomId).emit(Constants.grieve, Object.assign({}, server, { content: `${_user.name} connected`, timestamp: Date.now() }));
+    };
 
-    socket.join(roomId);
+    /**
+     * Allows the client to leave a specified room
+     * @param  {[String]} roomId  id of the room
+     * @return {[Unit]}           effectful void
+     */
+    const leave = (roomId, user) => {
+      socket.leave(roomId);
+      _rooms.splice(_rooms.indexOf(roomId), 1);
+      socket.broadcast.to(roomId).emit(Constants.grieve, Object.assign({}, server, { content: `${_user.name} disconnected`, timestamp: Date.now() }));
+    };
 
-    socket.broadcast.to(roomId).emit(Constants.greet, user);
-  };
+    /**
+     * Allows client to send message to any room they belong to
+     * @param  {[Socket]} socket  socket in which the action will take effect
+     * @param  {[Object]}   data  message information
+     * @return {[Unit]}     effectful void
+     */
+    const send = (data) => {
+      socket.broadcast.to(data.room).emit(Constants.message, { from: data.from, content: data.content, timestamp: data.timestamp });
+    };
 
-  /**
-   * Allows the client to leave a specified room
-   * @param  {[Socket]} socket  socket in which the action will take effect
-   * @param  {[String]} roomId  id of the room
-   * @return {[Unit]}           effectful void
-   */
-  const leave = (roomId, user) => {
-    socket.leave(roomId);
+    /**
+     * Allows client to know when a User logged in
+     * @param   {[Object]}  user  user
+     * @return  {[Unit]}          effectful void
+     */
+    const userEnter = (user) => {
+      _user = user;
+      users.push(user);
+      socket.broadcast.emit(Constants.userUpdate, users);
+    }
 
-    socket.broadcast.to(roomId).emit(Constants.grieve, user);
-  };
+    /**
+     * Allows client to know when a User logged out
+     * @param   {[Object]}  user  user
+     * @return  {[Unit]}          effectful void
+     */
+    const userLeave = (user) => {
+      _user = {};
+      users = users.filter(u => u.id != user.id);
+      socket.broadcast.emit(Constants.userUpdate, users);
+    }
 
-  /**
-   * Allows client to send message to any room they belong to
-   * @param  {[Socket]} socket  socket in which the action will take effect
-   * @param  {[Object]}   data  message information
-   * @return {[Unit]}     effectful void
-   */
-  const send = (data) => {    
-    socket.broadcast.to(data.room).emit(Constants.message, Object.assign(data, { timestamp: Date.now() }));
-  };
+    /**
+     * Allows client to create a chatroom
+     * @param  {[String]} room  name of the room
+     * @return {[Unit]}         effectful void
+     */
+    const chatroomCreate = (room) => {
+      fs.writeFileSync(chatroomsDat, rooms.concat([room]).join('\n'));
 
-  /**
-   * Allows client to know when a User logged in
-   * @param   {[Object]}  user  user
-   * @return  {[Unit]}          effectful void
-   */
-  const userEnter = (user) => {
-    users.push(user);
-    socket.broadcast.emit(Constants.userUpdate, users);
-  }
+      rooms = getChatroomData();
+      io.sockets.emit(Constants.roomUpdate, rooms)
+    }
 
-  /**
-   * Allows client to know when a User logged out
-   * @param   {[Object]}  user  user
-   * @return  {[Unit]}          effectful void
-   */
-  const userLeave = (user) => {
-    users = users.filter(u => u.id != user.id);
-    socket.broadcast.emit(Constants.userUpdate, users);
-  }
+    /**
+     * Allows client to delete a chatroom
+     * @param  {[String]} room  name of the room
+     * @return {[Unit]}         effectful void
+     */
+    const chatroomDelete = (room) => {
+      writeToChatroomData(rooms.filter(r => r !== room).join('\n'));
 
-  /**
-   * Allows client to create a chatroom
-   * @param  {[String]} room  name of the room
-   * @return {[Unit]}         effectful void
-   */
-  const chatroomCreate = (room) => {
-    rooms.push(room);    
-    fs.writeFileSync(chatroomsDat, rooms.join('\n'));
-    socket.emit(Constants.roomUpdate, rooms)
-  }
+      rooms = getChatroomData();
+      io.sockets.emit(Constants.roomUpdate, rooms)
+    }
 
-  /**
-   * Allows client to delete a chatroom
-   * @param  {[String]} room  name of the room
-   * @return {[Unit]}         effectful void
-   */
-  const chatroomDelete = (room) => {
-    const roomIdx = rooms.indexOf(room);
-    rooms.splice(roomIdx, 1);
+    /**
+     * Allows client to disconnect from the application
+     * @return  {[Unit]}  effectful void
+     */
+    const disconnect = () => {
+      _rooms.forEach(r => leave(r, _user));
+      userLeave(_user);
+    }
 
-    fs.writeFileSync(chatroomsDat, rooms.join('\n'));
-    socket.emit(Constants.roomUpdate, rooms)
-  }
-
-  // Adding listeners
-  socket
-    .on(Constants.send, send)
-    .on(Constants.join, join)
-    .on(Constants.leave, leave)
-    .on(Constants.userEnter, userEnter)
-    .on(Constants.userLeave, userLeave)
-    .on(Constants.chatroomCreate, chatroomCreate)
-    .on(Constants.chatroomDelete, chatroomDelete);
+    // Adding listeners
+    socket
+      .on(Constants.send, send)
+      .on(Constants.join, join)
+      .on(Constants.leave, leave)
+      .on(Constants.userEnter, userEnter)
+      .on(Constants.userLeave, userLeave)
+      .on(Constants.disconnect, disconnect)
+      .on(Constants.chatroomCreate, chatroomCreate)
+      .on(Constants.chatroomDelete, chatroomDelete);
 
 
-  // Set up defaults
-  socket
-    .emit(Constants.userUpdate, users)
-    .emit(Constants.roomUpdate, rooms);
+    // Set up defaults
+    socket
+      .emit(Constants.userUpdate, users)
+      .emit(Constants.roomUpdate, rooms);
+  })();
 }
 
-io.on(Constants.connection, connection);
+io.sockets.on(Constants.connection, connection);
